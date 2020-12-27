0f13eac013a9ebf4c2bff83e09ff8f86fbc902e4
DAHYUN-HAN <45094929+DAHYUN-HAN@users.noreply.github.com>
2020-12-27 15:42:42 +0900

    Create countpy_solution.py

---
+++ countpy_solution.py
@@ -0,0 +1,15 @@
+def solution(s):
+    p_count = 0
+    y_count = 0
+    for i in s:
+        if(i == 'P' or i =='p'):
+            p_count += 1
+        elif(i == 'Y' or i == 'y'):
+            y_count += 1
+            
+    if(p_count == y_count):
+        answer = True
+    else:
+        answer = False
+        
+    return answer
