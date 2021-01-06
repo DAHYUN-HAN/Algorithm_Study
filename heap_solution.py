b299d515f1bb58bee36781dd4944e1ae4f0b97b4
DAHYUN-HAN <45094929+DAHYUN-HAN@users.noreply.github.com>
2021-01-06 10:10:45 +0900

    Create heap_solution.py

---
+++ heap_solution.py
@@ -0,0 +1,12 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heap = []
+    for i in scoville:
+        heapq.heappush(heap, i)
+    while(heap[0] < K):
+        if(len(heap) == 1):
+            return -1
+        heapq.heappush(heap, heapq.heappop(heap)+ heapq.heappop(heap)*2)
+        answer += 1
+    return answer
