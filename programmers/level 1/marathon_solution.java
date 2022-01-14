6c807a6273337facc4f78943ffde0f0aa0065083
DAHYUN-HAN <45094929+DAHYUN-HAN@users.noreply.github.com>
2022-01-14 11:29:13 +0900

    Update marathon_solution.java

--- programmers/level 1/marathon_solution.java
+++ programmers/level 1/marathon_solution.java
@@ -18,3 +18,32 @@ class Solution {
         return answer;
     }
 }
+
+import java.util.HashMap;
+
+class Solution2 {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < participant.length; i++) {
+            if(map.get(participant[i]) == null) {
+                map.put(participant[i], 1);
+            }
+            else {
+                map.put(participant[i], map.get(participant[i]) + 1);
+            }
+        }
+        
+        for(int i = 0; i < completion.length; i++) {
+            map.put(completion[i], map.get(completion[i]) - 1);
+        }
+        
+        for(String key : map.keySet()){
+            if(map.get(key) != 0) {
+                return key;
+            }
+        }
+        
+        return "";
+    }
+}
